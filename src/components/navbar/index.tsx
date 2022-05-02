3b0db5a38439b5dee47c2e7985d398500f074def
kraubunas <kraubun@gmail.com>
2022-05-03 01:01:42 +0300

    added navbar login and visitor menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,25 +1,37 @@
 import {
   AppBar,
+  Box,
   Container,
   Toolbar,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
+import NavbarAuthMenu from './navbar-auth-menu';
 import NavbarLink from './navbar-link';
+import NavbarVisitorMenu from './navbar-visitor-menu';
+import AuthContext from '../../features/auth/auth-context';
 
-const Navbar: React.FC = () => (
-  <AppBar position="static" sx={{ bgcolor: 'darkBlue.main' }}>
-    <Container sx={{ px: { xs: 0, sm: 0 } }}>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Personal Blog
-        </Typography>
-        <NavbarLink to="/">Home</NavbarLink>
-        <NavbarLink to="/blog">Products</NavbarLink>
-        <NavbarLink to="/about">About</NavbarLink>
-      </Toolbar>
-    </Container>
-  </AppBar>
-);
+const Navbar: React.FC = () => {
+  const { loggedIn } = useContext(AuthContext);
+
+  return (
+
+    <AppBar position="static" sx={{ bgcolor: 'darkBlue.main' }}>
+      <Container sx={{ px: { xs: 0, sm: 0 } }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Personal Blog
+          </Typography>
+          <NavbarLink to="/">Home</NavbarLink>
+          <NavbarLink to="/blog">Products</NavbarLink>
+          <NavbarLink to="/about">About</NavbarLink>
+          <Box>
+            {loggedIn ? <NavbarAuthMenu /> : <NavbarVisitorMenu />}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navbar;
---
+++ src/components/navbar/navbar-auth-menu.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useRef, useContext } from 'react';
+import {
+  Avatar,
+  Popper,
+  Box,
+  Paper,
+  MenuList,
+  MenuItem,
+  Divider,
+  Typography,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import deepOrange from '@mui/material/colors/deepOrange';
+import AuthContext from '../../features/auth/auth-context';
+
+const NavbarAuthMenu: React.FC = () => {
+  const navigate = useNavigate();
+  const { logout, user } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const popperAnchorRef = useRef<HTMLDivElement>(null);
+
+  const handleNavigate = (route: string) => {
+    setMenuOpen(false);
+    navigate(route);
+  };
+
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <Box
+      ref={popperAnchorRef}
+      sx={{ display: 'inline-flex', alignItems: 'center', height: 64 }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          cursor: 'pointer',
+        }}
+        onClick={handleMenuOpen}
+      >
+        <Typography sx={{ mr: 2, userSelect: 'none' }}>{user?.email}</Typography>
+        <Avatar sx={{ bgcolor: deepOrange[500] }}>U</Avatar>
+      </Box>
+      <Popper
+        placement="bottom-end"
+        anchorEl={popperAnchorRef.current}
+        open={menuOpen}
+        sx={{ zIndex: 'tooltip' }}
+      >
+        <Paper elevation={3}>
+          <MenuList>
+            <MenuItem onClick={() => handleNavigate('/profile')}>
+              ProfilePage
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={logout}>
+              Atsijungti
+            </MenuItem>
+          </MenuList>
+        </Paper>
+      </Popper>
+    </Box>
+  );
+};
+
+export default NavbarAuthMenu;
