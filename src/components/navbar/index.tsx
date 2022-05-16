70bce7b59d7c87a6c767fd5a0e48e9b966111a21
kraubunas <kraubun@gmail.com>
2022-05-16 19:19:07 +0300

    added working cart button

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,24 +2,30 @@ import {
   AppBar,
   Box,
   Container,
+  Drawer,
   IconButton,
   Toolbar,
   Typography,
 } from '@mui/material';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import NavbarAuthMenu from './navbar-auth-menu';
 import NavbarLink from './navbar-link';
 import NavbarVisitorMenu from './navbar-visitor-menu';
 import AuthContext from '../../features/auth/auth-context';
+import { CartItemType } from '../../types/cart-item-type';
 
 const Navbar: React.FC = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { loggedIn } = useContext(AuthContext);
+
+  const getTotalItems = (items: CartItemType[]) => null;
 
   return (
 
-    <AppBar position="static" sx={{ bgcolor: 'darkBlue.main' }}>
+    <AppBar position="fixed" sx={{ bgcolor: 'darkBlue.main' }}>
       <Container sx={{ px: { xs: 0, sm: 0 } }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -31,8 +37,11 @@ const Navbar: React.FC = () => {
           <Box sx={{ display: 'flex' }}>
             {loggedIn ? <NavbarAuthMenu /> : <NavbarVisitorMenu />}
           </Box>
-          <IconButton aria-label="cart">
-            <Badge badgeContent={1} color="primary">
+          <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+            cart goes here
+          </Drawer>
+          <IconButton aria-label="cart" sx={{ zIndex: 100 }} onClick={() => setCartOpen(true)}>
+            <Badge badgeContent={getTotalItems(cartItems)} color="primary">
               <ShoppingCartIcon htmlColor="white" />
             </Badge>
           </IconButton>
