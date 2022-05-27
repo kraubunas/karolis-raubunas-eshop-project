6048085afc37f8ba1ee1ba0a1d98f2b93f8b938a
kraubunas <kraubun@gmail.com>
2022-05-27 13:15:10 +0300

    deleted unnecessary code

--- src/store/features/cart/cart-reducer.ts
+++ src/store/features/cart/cart-reducer.ts
@@ -11,46 +11,6 @@ const initialState: CartState = {
   // productItems: [],
 };
 
-// const cartReducer: Reducer<CartState, CartAction> = (state = initialState, action) => {
-//   switch (action.type) {
-//     case CartActionType.ADD_TO_CART:
-//       // const item = state.productItems.find((prod) => prod.id === action.payload.itemId);
-//       // eslint-disable-next-line no-unneeded-ternary
-//       // const inCart = state.cartItems.find((itm) => ((itm.id === action.payload.itemId) ? true : false));
-//       return {
-//         ...state,
-//         cartItems: [...state.productItems, {
-//           productId: createId(),
-//           cartItemId: action.payload.itemId,
-//           amount: action.payload.amount,
-//         }],
-//         // ...state,
-//         // cart: inCart
-//         //   ? state.cartItems.map((itm) => (itm.id === action.payload.itemId
-//         //     ? { ...itm, qty: itm.amount + 1 }
-//         //     : itm))
-//         //   : [...state.cartItems, { ...item, qty: 1 }],
-//       };
-//     case CartActionType.REMOVE_FROM_CART:
-//       return {
-//         ...state,
-//         cartItems: state.cartItems.filter((itm) => (itm.itemId !== action.payload.itemId)),
-//         // cart: state.cartItems.filter((itm) => itm.id !== action.payload.itemId),
-//       };
-//     case CartActionType.CART_UPDATE_ITEM:
-//       return {
-//         ...state,
-//         cartItems: state.cartItems.map((itm) => (
-//           itm.itemId === action.payload.itemId
-//             ? { ...itm, amount: action.payload.amount }
-//             : itm
-//         )),
-//         // cart: state.cartItems.map((itm) => (itm.id === action.payload.itemId ? { ...itm, qty: action.payload.amount } : itm)),
-//       };
-//     default: return state;
-//   }
-// };
-
 const cartReducer: Reducer<CartState, CartAction> = (state = initialState, action) => {
   switch (action.type) {
     case CartActionType.ADD_TO_CART: {
