49455640f9e5539ee459bf7bae8252b0fdeba22e
kraubunas <kraubun@gmail.com>
2022-06-01 16:26:43 +0300

    created add product form

--- src/pages/profile-page/index.tsx
+++ src/pages/profile-page/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-  Typography, Container,
+  Typography, Container, Paper, TextField, Button, FormControl, FormLabel, RadioGroup, FormControlLabel,
 } from '@mui/material';
+import Radio from '@mui/material/Radio';
 import { useRootSelector } from '../../store/hooks';
 import { selectUser } from '../../store/selectors';
 
@@ -9,14 +10,41 @@ const ProfilePage: React.FC = () => {
   const user = useRootSelector(selectUser);
 
   return (
-    <Container>
+    <Container sx={{
+      display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+    }}
+    >
       <Typography
         component="h1"
         variant="h3"
-        sx={{ textAlign: 'center', mt: 15 }}
+        sx={{
+          textAlign: 'center', mt: 15,
+        }}
       >
         {`Hello, ${user?.email}`}
       </Typography>
+      <Paper sx={{
+        display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '600px', mt: 3, py: 3,
+      }}
+      >
+        <Typography component="h1" variant="h4" sx={{ mb: 2 }}>Create new product</Typography>
+        <Container sx={{
+          mt: 1, display: 'flex', flexDirection: 'column', gap: 2,
+        }}
+        >
+          <TextField label="Product name" />
+          <TextField label="Image URL" />
+          <TextField label="Price" />
+          <FormControl>
+            <FormLabel>Category</FormLabel>
+            <RadioGroup row>
+              <FormControlLabel value="candlestick" control={<Radio />} label="Candlestick" />
+              <FormControlLabel value="candle" control={<Radio />} label="Candle" />
+            </RadioGroup>
+          </FormControl>
+          <Button variant="contained">Add product</Button>
+        </Container>
+      </Paper>
     </Container>
   );
 };
