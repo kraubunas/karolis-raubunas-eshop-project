e3635a6fb4117132d9f26d487f781c659a11026d
kraubunas <kraubun@gmail.com>
2022-06-13 15:33:07 +0300

    added API_SERVER to products action creators

--- src/store/features/products/products-action-creators.ts
+++ src/store/features/products/products-action-creators.ts
@@ -5,11 +5,11 @@ import { AppAction } from '../../redux-types';
 import {
   ProductFetchItemsLoadingAction,
   ProductFetchItemsSuccessAction,
-  ProductFetchItemsFailureAction,
   ProductsActionType,
 } from './products-types';
 import Product from '../../../types/products';
-import pause from '../../../helpers/pause';
+
+const API_SERVER = process.env.REACT_APP_API_SERVER;
 
 const productFetchItemsLoadingAction: ProductFetchItemsLoadingAction = {
   type: ProductsActionType.PRODUCT_FETCH_ITEMS_LOADING,
@@ -23,7 +23,7 @@ const createProductFecthItemsSuccessAction = (items: Product[]): ProductFetchIte
 export const productFetchItemsAction = async (dispatch: Dispatch<AppAction>): Promise<void> => {
   dispatch(productFetchItemsLoadingAction);
 
-  const { data } = await axios.get<Product[]>('http://localhost:8000/products');
+  const { data } = await axios.get<Product[]>(`${API_SERVER}/products`);
   const productFecthItemsSuccessAction = createProductFecthItemsSuccessAction(data);
   dispatch(productFecthItemsSuccessAction);
 };
